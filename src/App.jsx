81079e251d2a8baf4f869376655c90062ffeee38
Moyomade <adegbitemoyomade2004@gmail.com>
2024-01-31 23:38:10 +0100

    Updated App.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
 
 export default function App() {
 	return (
 		<div>
 			<Navbar />
+			<Hero />
 		</div>
 	);
 }
