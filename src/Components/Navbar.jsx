0ccaa06533cb5d3ac9905f8452dbe9d983eb0b04
Moyomade <adegbitemoyomade2004@gmail.com>
2024-02-02 17:22:16 +0100

    Navigation bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,14 +6,16 @@ import { LuPhoneCall } from "react-icons/lu";
 import { RiArrowDropDownLine } from "react-icons/ri";
 
 function Navbar() {
+	const [openMenu, setOpenMenu] = useState(false);
 	const [open, setOpen] = useState(false);
 	const [activeLink, setActiveLink] = useState(null);
 
 	const handleClick = (index) => {
 		setActiveLink(index);
+		setOpenMenu(false);
 	};
 	return (
-		<div className="flex items-center justify-evenly p-4">
+		<div className="flex flex-col md:flex-row items-center justify-evenly p-4">
 			{/* Logo */}
 			<h1 className="text-xl font-bold">
 				<span className="bg-[#39DB4A] bg-origin-border rounded-md px-1.5 text-white">
@@ -21,9 +23,22 @@ function Navbar() {
 				</span>
 				ushi
 			</h1>
+			{/* Menu Icon for Small Screens */}
+			<div className="md:hidden">
+				<button
+					onClick={() => setOpenMenu(!openMenu)}
+					className="text-2xl hover:bg-[#eae8e8] p-1 rounded-md focus:outline-none"
+				>
+					☰
+				</button>
+			</div>
 
 			{/* Navigation Links */}
-			<div className="flex items-center space-x-3 font-bold">
+			<div
+				className={`flex md:flex  items-center space-x-3 font-bold  ${
+					openMenu ? "flex-col md:flex-row" : "hidden"
+				}`}
+			>
 				<a
 					href=""
 					className={`p-4 ${
@@ -73,7 +88,7 @@ function Navbar() {
 			</div>
 
 			{/* Search, cart, contact */}
-			<div className="flex space-x-6">
+			<div className="flex mt-4 md:mt-0 space-x-6 ">
 				<div className="text-2xl">
 					<FiSearch />
 				</div>
