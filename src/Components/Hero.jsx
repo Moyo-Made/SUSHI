77ea7e82e77ac81f0f6e877b034227987ebd5179
Moyomade <adegbitemoyomade2004@gmail.com>
2024-02-04 23:31:55 +0100

    Hero.jsx component

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,9 +2,14 @@
 import React from "react";
 import playButton from "../assets/Polygon 1.png";
 import sushiImage from "../assets/MAIN SUSHI IMAGE.png";
+import Makizushi from "../assets/_cc226cf5-2147-430e-b16c-84644636b1fe-removebg-preview 1.png";
+import caliRoll from "../assets/_78d38fb4-559d-4cdb-822d-44ecdec0a7f8-removebg-preview 1.png";
+import { FaStar } from "react-icons/fa";
+import { FaRegStar } from "react-icons/fa6";
 
 function Hero() {
 	return (
+		// Left Side
 		<div className="flex">
 			<div className="flex-1 flex justify-center mt-20">
 				<div className="ml-34">
@@ -33,20 +38,62 @@ function Hero() {
 				</div>
 			</div>
 
+			{/* Right Side */}
 			<div className="flex-1 flex flex-col items-center z-50">
 				<img
 					src={sushiImage}
 					alt="Sushi Image"
-					width={500}
-					height={500}
-					className="mt-[-12px]"
+					width={450}
+					height={4500}
+					className="mt-[-20px] mr-24"
 				/>
-				<div className="flex flex-row items-center space-x-7 ">
-					<div className="bg-[#f9f9f9fd]  w-40 h-14 ml-20  mt-[-200px] shadow-2xl rounded-xl -z-10">
-						<h4>Sushi</h4>
+				<div className="flex flex-row items-center space-x-7 mr-24">
+					{/* Makizushi */}
+					<div className="flex items-center space-x-3 bg-[#f9f9f9fd] space-y-2 w-52 h-24 ml-20  mt-[-180px] shadow-2xl rounded-xl -z-10">
+						<img
+							width={50}
+							height={50}
+							src={Makizushi}
+							alt="Makizushi"
+							className="ml-5"
+						/>
+						<div className="flex-1">
+							<h3 className="font-bold">Makizushi</h3>
+							<div className="flex flex-row text-yellow-400">
+								<FaStar />
+								<FaStar />
+								<FaStar />
+								<FaRegStar />
+								<FaRegStar />
+							</div>
+							<h3 className="font-bold">
+								<span className="text-red-500 font-medium">$</span>18.00
+							</h3>
+						</div>
 					</div>
-					<div className=" bg-[#fdfafaa1]  mt-[-200px] w-40 h-14 shadow-2xl rounded-xl -z-10">
-						<h4>Sushi</h4>
+
+					{/* CaliRoll */}
+					<div className="flex items-center space-x-3 bg-[#f9f9f9fd] space-y-2 w-52 h-24 ml-20  mt-[-180px] shadow-2xl rounded-xl -z-10">
+						<img
+							width={50}
+							height={50}
+							src={caliRoll}
+							alt="Cali Roll"
+							className="ml-5"
+						/>
+						<div className="flex-1">
+							<h3 className="font-bold">California roll</h3>
+							<div className="flex flex-row text-yellow-400">
+								<FaStar />
+								<FaStar />
+								<FaStar />
+								<FaStar />
+								<FaRegStar />
+							</div>
+							<h3 className="font-bold">
+								<span className="text-red-500 font-medium">$</span>23.00
+							</h3>
+						</div>
 					</div>
 				</div>
 			</div>
