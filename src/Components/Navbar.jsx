b7ba9accbe66b91f96ea89060bc3fed0a7fea7eb
Moyomade <adegbitemoyomade2004@gmail.com>
2024-02-12 23:25:29 +0100

    Code: navigation bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,28 +1,28 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 import { FiSearch } from "react-icons/fi";
 import { RiShoppingBagLine } from "react-icons/ri";
 import { LuPhoneCall } from "react-icons/lu";
-// import { RiArrowDropDownLine } from "react-icons/ri";
+import { IoMenu } from "react-icons/io5";
+import { IoCloseSharp } from "react-icons/io5";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 function Navbar() {
-	// const [openMenu, setOpenMenu] = useState(false);
+	const location = useLocation();
 	const [open, setOpen] = useState(false);
-	const [activeLink, setActiveLink] = useState(null);
 
-	const handleClick = (link) => {
-		setActiveLink(link);
-		// setOpenMenu(false);
-	};
 	const Links = [
 		{ name: "Home", link: "/" },
-		{ name: "About", link: "/about" },
-		{ name: "Services", link: "/services" },
-		{ name: "Menu", link: "/Menu" },
+		{ name: "Menu", link: "/", icon: <RiArrowDropDownLine /> },
+		{ name: "About Us", link: "/" },
+		{ name: "Services", link: "/", icon: <RiArrowDropDownLine /> },
+		{ name: "Offers", link: "/" },
 	];
 	return (
 		<div className="shadow-md w-full fixed top-0 left-0">
 			<div className="md:flex items-center justify-evenly bg-white py-4 md:px-12 px-10">
+				{/* Logo */}
 				<div className="font-bold text-2xl cursor-pointer flex items-center">
 					<span>
 						<span className="bg-[#39DB4A] bg-origin-border rounded-md px-1.5 text-white">
@@ -31,22 +31,41 @@ function Navbar() {
 						ushi
 					</span>
 				</div>
-				<ul className="md:flex md:items-center ">
+
+				{/* Menu Icon for Small Screens */}
+				<div
+					onClick={() => setOpen(!open)}
+					className="text-3xl absolute right-4 top-6 cursor-pointer md:hidden"
+				>
+					{open ? <IoCloseSharp /> : <IoMenu />}
+				</div>
+				<ul
+					className={`md:flex md:items-center md:pb-0 pb-3 absolute md:static bg-white
+								md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all
+								duration-500 ease-in ${
+									open ? "top-20 opacity-100" : "top-[-490px]"
+								} md:opacity-100`}
+				>
 					{Links.map((Link) => (
-						<li key={Link.name} className="md:ml-8 text-lg">
+						<li key={Link.name} className="md:ml-8 text-md md:my-0 my-7">
 							<a
 								href={Link.link}
-								className={`text-gray-800 hover:text-[#39DB4A] ${
-									activeLink === Link.name ? "text-[#39DB4A]" : "text-red-500"
+								className={`text-gray-800 flex ${
+									location.pathname.startsWith(Link.link)
+										? "text-[#39DB4A]"
+										: ""
 								}`}
-								onClick={() => handleClick(Link.name)}
 							>
 								{Link.name}
+								<span className="text-[#39DB4A] text-2xl mt-[2px]">
+									{Link.icon}
+								</span>
 							</a>
 						</li>
 					))}
 				</ul>
 
+				{/* Search, cart, contact */}
 				<div className="flex mt-4 md:mt-0 space-x-6 ">
 					<div className="text-2xl">
 						<FiSearch />
@@ -70,17 +89,6 @@ function Navbar() {
 					</div>
 				</div>
 			</div>
-			{/* Logo */}
-
-			{/* Menu Icon for Small Screens */}
-			{/* <div className="md:hidden">
-				<button
-					onClick={() => setOpenMenu(!openMenu)}
-					className="text-2xl hover:bg-[#eae8e8] p-1 rounded-md focus:outline-none"
-				>
-					☰
-				</button>
-			</div> */}
 
 			{/* Navigation Links */}
 			{/* <div
@@ -149,7 +157,7 @@ function Navbar() {
 						</div>
 					</div>
 				</div>
-
+ 
 				<div className="button">
 					<div className="mr-2">
 						<LuPhoneCall />
