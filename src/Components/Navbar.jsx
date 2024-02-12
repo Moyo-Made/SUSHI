60e1f6b7ba0484d6aab1c32cef49f913e5ef08e9
Moyomade <adegbitemoyomade2004@gmail.com>
2024-02-12 18:44:26 +0100

    Code: navigation bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,38 +3,87 @@ import React, { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import { RiShoppingBagLine } from "react-icons/ri";
 import { LuPhoneCall } from "react-icons/lu";
-import { RiArrowDropDownLine } from "react-icons/ri";
+// import { RiArrowDropDownLine } from "react-icons/ri";
 
 function Navbar() {
-	const [openMenu, setOpenMenu] = useState(false);
+	// const [openMenu, setOpenMenu] = useState(false);
 	const [open, setOpen] = useState(false);
 	const [activeLink, setActiveLink] = useState(null);
 
-	const handleClick = (index) => {
-		setActiveLink(index);
-		setOpenMenu(false);
+	const handleClick = (link) => {
+		setActiveLink(link);
+		// setOpenMenu(false);
 	};
+	const Links = [
+		{ name: "Home", link: "/" },
+		{ name: "About", link: "/about" },
+		{ name: "Services", link: "/services" },
+		{ name: "Menu", link: "/Menu" },
+	];
 	return (
-		<div className="flex flex-col items-center justify-evenly p-2 shadow-md w-full md:flex-row">
+		<div className="shadow-md w-full fixed top-0 left-0">
+			<div className="md:flex items-center justify-evenly bg-white py-4 md:px-12 px-10">
+				<div className="font-bold text-2xl cursor-pointer flex items-center">
+					<span>
+						<span className="bg-[#39DB4A] bg-origin-border rounded-md px-1.5 text-white">
+							S
+						</span>
+						ushi
+					</span>
+				</div>
+				<ul className="md:flex md:items-center ">
+					{Links.map((Link) => (
+						<li key={Link.name} className="md:ml-8 text-lg">
+							<a
+								href={Link.link}
+								className={`text-gray-800 hover:text-[#39DB4A] ${
+									activeLink === Link.name ? "text-[#39DB4A]" : "text-red-500"
+								}`}
+								onClick={() => handleClick(Link.name)}
+							>
+								{Link.name}
+							</a>
+						</li>
+					))}
+				</ul>
+
+				<div className="flex mt-4 md:mt-0 space-x-6 ">
+					<div className="text-2xl">
+						<FiSearch />
+					</div>
+					<div className="relative">
+						<div className="text-2xl" onClick={() => setOpen(!open)}>
+							<RiShoppingBagLine />
+							<div className="w-3 h-3 bg-[#39DB4A] rounded-full flex items-center justify-center absolute top-0 right-0 text-white">
+								<span className="text-sm">8</span>
+							</div>
+						</div>
+					</div>
+
+					<div className="button">
+						<div className="mr-2">
+							<LuPhoneCall />
+						</div>
+						<div className="font-semibold">
+							<h4>Contact</h4>
+						</div>
+					</div>
+				</div>
+			</div>
 			{/* Logo */}
-			<h1 className="text-xl font-bold">
-				<span className="bg-[#39DB4A] bg-origin-border rounded-md px-1.5 text-white">
-					S
-				</span>
-				ushi
-			</h1>
+
 			{/* Menu Icon for Small Screens */}
-			<div className="md:hidden">
+			{/* <div className="md:hidden">
 				<button
 					onClick={() => setOpenMenu(!openMenu)}
 					className="text-2xl hover:bg-[#eae8e8] p-1 rounded-md focus:outline-none"
 				>
 					☰
 				</button>
-			</div>
+			</div> */}
 
 			{/* Navigation Links */}
-			<div
+			{/* <div
 				className={`flex md:flex  items-center space-x-3 font-bold  ${
 					openMenu ? "flex-col md:flex-row" : "hidden"
 				}`}
@@ -85,10 +134,10 @@ function Navbar() {
 				>
 					Offers
 				</a>
-			</div>
+			</div> */}
 
 			{/* Search, cart, contact */}
-			<div className="flex mt-4 md:mt-0 space-x-6 ">
+			{/* <div className="flex mt-4 md:mt-0 space-x-6 ">
 				<div className="text-2xl">
 					<FiSearch />
 				</div>
@@ -109,7 +158,7 @@ function Navbar() {
 						<h4>Contact</h4>
 					</div>
 				</div>
-			</div>
+			</div> */}
 		</div>
 	);
 }
