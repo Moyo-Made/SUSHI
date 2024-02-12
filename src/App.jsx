0919937dba1e7d221ee3b57bc2a7594dc90b09f2
Moyomade <adegbitemoyomade2004@gmail.com>
2024-02-12 23:26:40 +0100

    Code: updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import Hero from "./Components/Hero";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
 
 export default function App() {
 	return (
+		<Router>
 		<div className="bg-[#ffffff6d]">
 			<Navbar />
 			<Hero />
 		</div>
+		</Router>
 	);
 }
