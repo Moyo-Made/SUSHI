ab9e96996a7374ac905a93a2e73ef9a84568ce85
Moyomade <adegbitemoyomade2004@gmail.com>
2024-02-20 16:51:49 +0100

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import Hero from "./Components/Hero";
 import Categories from "./Components/Categories";
 import Subcategory from "./Components/Subcategory";
 import Testimonials from "./Components/Testimonials";
+import Services from "./Components/Services";
+import Footer from "./Components/Footer";
 
 export default function App() {
 	return (
 		<Router>
-		<div className="bg-[#ffffff6d]">
-			<Navbar />
-			<Hero />
-			<Categories />
-			<Subcategory />
-			<Testimonials />
-		</div>
+			<div className="bg-[#ffffff6d] overflow-x-hidden">
+				<Navbar />
+				<Hero />
+				<Categories />
+				<Subcategory />
+				<Testimonials />
+				<Services />
+				<Footer />
+			</div>
 		</Router>
 	);
 }
