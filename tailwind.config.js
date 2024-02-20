0a418379167bb7538583f8ac0f4bbe369ff68f44
Moyomade <adegbitemoyomade2004@gmail.com>
2024-02-20 17:13:57 +0100

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,24 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		screens:{
-			'sm': '640px',
+		screens: {
+			sm: "640px",
 			// => @media (min-width: 640px) { ... }
 
-			'md': '768px',
+			md: "768px",
 			// => @media (min-width: 768px) { ... }
 
-			'lg': '1024px',
+			lg: "1024px",
 			// => @media (min-width: 1024px) { ... }
 
-			'xl': '1280px',
+			xl: "1280px",
 			// => @media (min-width: 1280px) { ... }
 		},
-		extend: {},
+		extend: {
+			fontFamily: {
+				body: ["Inter"],
+			},
+		},
 	},
 	plugins: [],
 };
